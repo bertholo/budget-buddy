632b4478b27227e466eeaa9e22cc40b75a99947f
Joao Bertholo <joao.bertholo@gmail.com>
2024-10-29 09:21:27 +0000

    ajusted path

--- back-end/app.js
+++ back-end/app.js
@@ -11,9 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://budbuddy.netlify.app'], // Allow both localhost and the production URL
-    methods: ['GET', 'POST', 'DELETE', 'PUT'], // Specify methods your server supports
-    credentials: true // Allow cookies if you need them
+    origin: ['http://localhost:3000', 'https://budbuddy.netlify.app'],
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    credentials: true
 };
 
 
@@ -21,6 +21,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Budget Buddy API!');
+});
 
 app.use('/api/v1', transactionsRouter);
 
